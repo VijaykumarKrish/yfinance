71f341922aa07a4c61e2398e2bb1b6bc3af2fcf6
VijaykumarKrish <90506765+VijaykumarKrish@users.noreply.github.com>
2025-04-07 17:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,13 @@ def get_stock_info():
         return jsonify(info)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+        
+@app.route('/api/stocks', methods=['GET'])
+def get_stocks():
+    stocks = ["TATAMOTORS.NS","POWERGRID.NS","BEL.NS","ENGINERSIN.NS","GAIL.NS","IRFC.NS","BANKBARODA.NS","BHEL.NS","OLAELEC.NS","IRCON.NS"]  # Example: Apple Inc.
+
+    stock_data = fetch_stock_data(stocks)
+    return jsonify(stock_data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
